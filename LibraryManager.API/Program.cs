3b8d705b51df22a6f2e035efd9f852fbc3778d35
k-felipe <felipegkubiak@outlook.com>
2025-01-21 15:59:23 -0300

    Configuração do DBContext.

--- LibraryManager.API/Program.cs
+++ LibraryManager.API/Program.cs
@@ -1,3 +1,7 @@
+using LibraryManager.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<LibraryManagerDbContext> (options =>
+    options.UseInMemoryDatabase("library_manager"));
+
+
 
 var app = builder.Build();
 
--- LibraryManager.Infrastructure/Persistence/LibraryManagerDbContext.cs
+++ LibraryManager.Infrastructure/Persistence/LibraryManagerDbContext.cs
@@ -1,20 +1,57 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LibraryManager.Core.Entities;
 
 namespace LibraryManager.Infrastructure.Persistence
 {
     public class LibraryManagerDbContext : DbContext
     {
-        //criar conexao
+        public LibraryManagerDbContext(DbContextOptions<LibraryManagerDbContext> options) : base(options) { }
 
         public DbSet<Book> Books { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Loan> Loans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(u =>
+            {
+                u.HasKey(u => u.Id);
+
+                u.HasMany(l => l.Loans)
+                .WithOne(l => l.User)
+                .HasForeignKey(l => l.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Book>(b =>
+            {
+                b.HasKey(b => b.Id);
+
+                b.HasMany(l => l.Loans)
+                .WithOne(l => l.Book)
+                .HasForeignKey(l => l.BookId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            });
+
+            modelBuilder.Entity<Loan>(l =>
+            {
+                l.HasKey(l => l.Id);
+
+                l.HasOne(l => l.User)
+                .WithMany(u => u.Loans)
+                .HasForeignKey(l => l.UserId) 
+                .OnDelete(DeleteBehavior.Restrict);
+
+                l.HasOne(l => l.Book)
+                .WithMany(b => b.Loans)
+                .HasForeignKey(l => l.BookId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            });
+
+            base.OnModelCreating(modelBuilder); 
+        }
 
 
     }
